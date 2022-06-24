2c43f23ca195907b9de6636e97276b0feadcc8b8
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-06-24 10:10:00 +0530

    :sparkles: added status bar and styling

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar as rnStatusBar } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <View style={styles.container}>
+      <HomeScreen />
+      <StatusBar />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: rnStatusBar.currentHeight,
   },
 });
