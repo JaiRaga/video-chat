401eeabaa98f18726ac447cc7e9f12cacacc833a
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-06-24 10:08:35 +0530

    :zap: imported components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Header from '../components/Header';
+import Searchbar from '../components/Searchbar';
+import MenuButtons from '../components/MenuButtons';
+import ContactsMenu from '../components/ContactsMenu';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Header />
+      <Searchbar />
+      <MenuButtons />
+      <ContactsMenu />
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1e1e1e",
+    padding: 5
+  }
+});
