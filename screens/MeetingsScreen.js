0eb48ca96efd7654c2b3a6ce2c5e5a9029197f18
JaiRaga <44367062+JaiRaga@users.noreply.github.com>
2022-07-06 17:26:23 +0530

    :sparkles: setup socket.io on the client

--- screens/MeetingsScreen.js
+++ screens/MeetingsScreen.js
@@ -1,42 +1,36 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import StartMeeting from '../components/StartMeeting';
+import { API_URL } from '../constants/api';
 
 const MeetingsScreen = () => {
+  const [hasConnection, setConnection] = useState(false);
   const [name, setName] = useState('');
   const [roomId, setRoomId] = useState('');
+
+  useEffect(() => {
+    const socket = io(API_URL, { transports: ['websocket'] });
+    socket.io.on('open', () => {
+      console.log('client connected');
+      setConnection(true);
+    });
+    socket.io.on('close', () => setConnection(false));
+
+    return () => {
+      socket.disconnect();
+      socket.removeAllListeners();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <View style={styles.info}>
-          <TextInput
-            placeholder="Enter Name"
-            placeholderTextColor={'#888888'}
-            style={styles.textInput}
-            value={name}
-            onChangeText={setName}
-          />
-        </View>
-        <View style={styles.info}>
-          <TextInput
-            placeholder="Enter Room Name or Id"
-            placeholderTextColor={'#888888'}
-            style={styles.textInput}
-            value={roomId}
-            onChangeText={setRoomId}
-          />
-        </View>
-        <View style={styles.btnContainer}>
-          <TouchableOpacity onPress={() => {}} style={styles.meetingBtn}>
-            <Text style={styles.meetingBtnText}>Start Meeting</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <StartMeeting
+        name={name}
+        setName={setName}
+        roomId={roomId}
+        setRoomId={setRoomId}
+      />
     </View>
   );
 };
@@ -47,36 +41,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121212',
-  },
-  inputContainer: {},
-  info: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#414144',
-    borderTopWidth: 1,
-    borderBottomWidth: 1,
-    borderColor: '#646464',
-    padding: 12,
-    justifyContent: 'center',
-  },
-  textInput: {
-    color: 'white',
-    fontSize: 18,
-  },
-  btnContainer: {
-    alignItems: 'center',
-  },
-  meetingBtn: {
-    width: 350,
-    marginTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2155CD',
-    height: 50,
-    borderRadius: 25,
-  },
-  meetingBtnText: {
-    color: 'white',
-    fontWeight: 'bold',
   },
 });
